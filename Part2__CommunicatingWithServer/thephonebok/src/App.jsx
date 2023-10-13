0b212fc2f0807c5be45aa56e58bf9e749a11aa0b
rgoldsmith33703 <rgolds33703@outlook.com>
2023-10-13 19:03:47 -0400

    Completed Exercise 2.9 The Phonebook Step 4

--- Part2__CommunicatingWithServer/thephonebok/src/App.jsx
+++ Part2__CommunicatingWithServer/thephonebok/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '555-555-5555'
-     }
-  ])
+  const [persons, setPersons] = useState([{
+    name: '',
+    number: ''
+  }])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
 
   function addNewName(e) {
     e.preventDefault()
@@ -23,10 +22,28 @@ const App = () => {
     setNewNumber('')
   }
 
+function numbersList(search) {
+  const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()));
+  const listItems = filteredPersons.map(person => (
+    <p key={person.name}>
+      {person.name} {person.number}
+    </p>
+  ));
+  return listItems;
+}
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>filter shown with
+        <input 
+          value={search}
+          onChange={e =>
+            (setSearch(e.target.value))}
+        />
+      </p>
       <form>
+        <h2>add a new</h2>
         <div>
           name: <input
                   value={newName} 
@@ -49,11 +66,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person => (
-          <p key={person.name}>
-            {person.name} {person.number}
-          </p>
-        ))}
+        {numbersList(search)}  
     </div>
   )
 }
