789fcce10525da849aa7a42430ab1aa338048fad
rgoldsmith33703 <rgolds33703@outlook.com>
2023-10-15 23:50:02 -0400

    Correct key on list render

--- Part2__CommunicatingWithServer/thephonebok/src/components/NumbersList.jsx
+++ Part2__CommunicatingWithServer/thephonebok/src/components/NumbersList.jsx
@@ -2,7 +2,7 @@ export default function NumbersList({search, persons, deletePerson}) {
   function numbersList(search) {
     const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()));
     const listItems = filteredPersons.map(person => (
-      <p key={person.name}>
+      <p key={person.id}>
         {person.name} {person.number} 
         <button value={person.id} onClick={deletePerson}>Delete</button>
       </p>
