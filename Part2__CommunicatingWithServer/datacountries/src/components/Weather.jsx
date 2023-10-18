355cda870c6bd92ba0d09941c3dac6d2bed95b57
rgoldsmith33703 <rgolds33703@outlook.com>
2023-10-18 17:03:38 -0400

    api updated

--- Part2__CommunicatingWithServer/datacountries/src/components/Weather.jsx
+++ Part2__CommunicatingWithServer/datacountries/src/components/Weather.jsx
@@ -4,7 +4,7 @@ import axios from "axios"
 const Weather = ({ country }) => {
   const api_key = import.meta.env.VITE_OPENWEATHER_API_KEY
   const [ lat, lon ] = country.capitalInfo.latlng
-  const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${api_key}`
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`
 
 
   const [currentWeather, setCurrentWeather] = useState([])
