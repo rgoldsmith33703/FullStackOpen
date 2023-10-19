c5523fdff3d47abb410f76952e0d5b6694e50571
rgoldsmith33703 <rgolds33703@outlook.com>
2023-10-19 14:07:04 -0400

    Completed Exercise 2.20 Data for Countries Step 3

--- Part2__CommunicatingWithServer/datacountries/src/components/Weather.jsx
+++ Part2__CommunicatingWithServer/datacountries/src/components/Weather.jsx
@@ -1,25 +1,42 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback } from "react"
 import axios from "axios"
 
 const Weather = ({ country }) => {
+  const [currentWeather, setCurrentWeather] = useState({
+    temp: null,
+    wind: null,
+    icon: null
+  })
   const api_key = import.meta.env.VITE_OPENWEATHER_API_KEY
   const [ lat, lon ] = country.capitalInfo.latlng
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=imperial`
+  
 
+  const fetchWeatherData = useCallback(() => {
+    axios.get(`${apiUrl}`)
+          .then(res => res.data)
+          .then(data => setCurrentWeather(prevWeather => {
+            return { ...prevWeather,
+              temp: data.main.temp,
+              wind: data.wind.speed,
+              icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+             }
 
-  const [currentWeather, setCurrentWeather] = useState([])
+          }))
+  }, [apiUrl])  
 
   useEffect(() => {
-    axios.get(`${apiUrl}`)
-         .then(res => res.data)
-         .then(weather => setCurrentWeather(weather))
-  }, [apiUrl])
-
-  console.log(currentWeather)
+    fetchWeatherData()
+  }, [fetchWeatherData])
 
   return (
-    <h2>Current Weather</h2>
-  )
+    <>
+      <h2>Current Weather</h2>
+      <p>Temp: {currentWeather.temp}&deg;</p>
+      <img src={currentWeather.icon} alt="" />
+      <p>Wind Speed: {currentWeather.wind} MPH</p>
+    </>
+  )   
 }
 
 export default Weather
